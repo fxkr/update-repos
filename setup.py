776218235009c1adfd0f388f5c36e9bb9ba22fdd
Felix Kaiser <felix.kaiser@fxkr.net>
2013-06-16 23:06:26 +0200

    Fix build (include README.rst / LICENSE)

--- setup.py
+++ setup.py
@@ -6,6 +6,8 @@ setup(
     description='Update all repositories in a directory',
     author='Felix Kaiser',
     author_email='felix.kaiser@fxkr.net',
+    long_description=open('README.rst').read(),
+    license=open('LICENSE').read(),
     url='',
     packages=[],
     scripts=['update-repos'])
